2731db3cff93c7b8d89d499084bcdc5b990156ab
Huy Ho <tuanhuy0496@yahoo.com>
2020-05-13 11:59:21 +0700

    Add Build

---
+++ build/precache-manifest.f02b34032d22a9207e18f74141006eb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/static/media/no-img.6732bd42.png"
+  },
+  {
+    "revision": "9d8b4819c514224ab8febca23643140b",
+    "url": "/static/media/logo.9d8b4819.PNG"
+  },
+  {
+    "revision": "049e543be011e7e19c470001b68f51af",
+    "url": "/static/media/icon.049e543b.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d33e619ce4fe90317345",
+    "url": "/static/js/main.e2e2d8a3.chunk.js"
+  },
+  {
+    "revision": "cc9332fe224f48196abd",
+    "url": "/static/js/2.9d7b4437.chunk.js"
+  },
+  {
+    "revision": "d33e619ce4fe90317345",
+    "url": "/static/css/main.ebd12b50.chunk.css"
+  },
+  {
+    "revision": "8a774539f90127a4730b56d4b2632a64",
+    "url": "/index.html"
+  }
+];
